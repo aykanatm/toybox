0dccd45fa7df9f61b6ef4bbd32d88dfdd3aae243
Murat Aykanat <aykanatm@gmail.com>
2018-03-24 14:16:58 +0300

    Hystrix turbine added.

---
+++ toybox-server/toybox-hystrix-turbine/src/main/java/com/github/murataykanat/toybox/ToyboxHystrixTurbineApplication.java
@@ -0,0 +1,12 @@
+package com.github.murataykanat.toybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyboxHystrixTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyboxHystrixTurbineApplication.class, args);
+	}
+}
