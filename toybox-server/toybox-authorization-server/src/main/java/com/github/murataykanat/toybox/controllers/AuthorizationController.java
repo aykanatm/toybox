43e61e6fb97935e774e3eaf501147121c0a09c95
Murat Aykanat <aykanatm@gmail.com>
2018-04-29 17:42:40 +0300

    Mapping /me now returns the actual user object, not the principal.

--- toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/controllers/AuthorizationController.java
+++ toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/controllers/AuthorizationController.java
@@ -1,15 +1,26 @@
 package com.github.murataykanat.toybox.controllers;
 
+import com.github.murataykanat.toybox.models.User;
+import com.github.murataykanat.toybox.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @RestController
 public class AuthorizationController {
+    @Autowired
+    private UsersRepository usersRepository;
+
     // Returns the currently authenticated user
-    @RequestMapping("/user")
-    public Principal user(Principal user){
-        return user;
+    @RequestMapping({"/user", "/me"})
+    public User user(Principal user){
+        Optional<User> optionalUser = usersRepository.findByUsername(user.getName());
+        optionalUser
+                .orElseThrow(() -> new UsernameNotFoundException("Username '" + user.getName() + "' not found!"));
+        return optionalUser.get();
     }
 }
