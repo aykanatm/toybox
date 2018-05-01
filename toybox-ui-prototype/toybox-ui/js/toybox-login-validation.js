b11af91b6d19ca2a341c3cf69c3ce80cad9a7d3d
Murat Aykanat <aykanatm@gmail.com>
2018-05-01 19:23:35 +0300

    Prototype login page first iteration.

---
+++ toybox-ui-prototype/toybox-ui/js/toybox-login-validation.js
@@ -0,0 +1,23 @@
+$('.ui.form')
+  .form({
+    fields: {
+      name: {
+        identifier: 'username',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please provide a username.'
+          }
+        ]
+      },
+      skills: {
+        identifier: 'password',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please provide a password.'
+          }
+        ]
+      },      
+    }
+});
