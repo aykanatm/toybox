94b5d03cde25612423798b29d35f01ae186bf895
Murat Aykanat <aykanatm@gmail.com>
2018-05-04 13:30:33 +0300

    Added CORS configuration so it can be used from another domain.

---
+++ toybox-server/toybox-styling-service/src/main/java/com/github/murataykanat/toybox/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.github.murataykanat.toybox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/style/**").addResourceLocations("classpath:/static/style/");
+    }
+}
