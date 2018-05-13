7367fbd9859404a61a3ce906de48751b182bdaac
Murat Aykanat <aykanatm@gmail.com>
2018-05-13 15:32:53 +0300

    Selecting and de-selecting assets implemented.

--- toybox-server/toybox-styling-service/src/main/resources/static/js/file-view.js
+++ toybox-server/toybox-styling-service/src/main/resources/static/js/file-view.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var cards = $('.toybox-card');
     var gridListToggleButton = $('#grid-list-toggle-button');
 
     gridListToggleButton.click(function () {
@@ -9,5 +10,28 @@ $(document).ready(function () {
         else{
             gridListToggleButton[0].innerHTML = '<i class="grid layout icon"></i>';
         }
+    });
+
+    cards.click(function () {
+       var cardId = $(this).attr('id');
+       var cardClass = $(this).attr('class');
+       if(cardClass.indexOf('toybox-card-selected') !== -1){
+           $(this).removeClass('toybox-card-selected');
+           var cornerLabel = $(this)[0].getElementsByClassName('toybox-corner-label')[0];
+           $(cornerLabel).addClass('hidden');
+           console.log("Asset with ID " + cardId + ' de-selected.');
+       }
+       else{
+           $(this).addClass('toybox-card-selected');
+           var cornerLabel = $(this)[0].getElementsByClassName('toybox-corner-label')[0];
+           $(cornerLabel).removeClass('hidden');
+           console.log("Asset with ID " + cardId + ' selected.');
+       }
+
+
+
+
+       // 0 1px 3px 0 #3a7dc8, 0 0 0 1px #3a7dc8
+       // 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5
     });
 });
