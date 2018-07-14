0d66123cb316fbd908e06c2902cfdae79c7acf73
Murat Aykanat <aykanatm@gmail.com>
2018-07-14 19:50:41 +0300

    ServiceConfig is renamed to SecurityConfig.

--- toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/SecurityConfig.java
+++ toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/SecurityConfig.java
@@ -3,27 +3,30 @@ package com.github.murataykanat.toybox.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
-public class ServiceConfig extends GlobalAuthenticationConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService customUserDetailsService;
 
     @Override
-    public void init(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService);
     }
 
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.antMatcher("/**")
+        httpSecurity
+                .requestMatchers()
+                    .antMatchers("/login", "/oauth/authorize")
+                    .and()
                 .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
-                .anyRequest().authenticated()
-                .and().exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"));
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll();
     }
 }
