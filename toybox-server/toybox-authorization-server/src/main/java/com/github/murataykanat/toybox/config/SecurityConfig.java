6d12ebea52065a182d7a399c91549b8fb246c5af
Murat Aykanat <aykanatm@gmail.com>
2018-07-21 17:11:23 +0300

    Incoming passwords are now hased to compare them with other hashed passwords in the database.

--- toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/SecurityConfig.java
+++ toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package com.github.murataykanat.toybox.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService customUserDetailsService;
 
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService);
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
