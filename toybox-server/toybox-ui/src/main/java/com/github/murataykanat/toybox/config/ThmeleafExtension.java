c6ea69fc8acde3de719fdff39539b52c4b657d52
Murat Aykanat <aykanatm@gmail.com>
2018-07-22 15:56:19 +0300

    UI components of the main UI service is now loaded externally from TOYBOX_HOME/ui

---
+++ toybox-server/toybox-ui/src/main/java/com/github/murataykanat/toybox/config/ThmeleafExtension.java
@@ -0,0 +1,26 @@
+package com.github.murataykanat.toybox.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+@Configuration
+public class ThmeleafExtension {
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+    @PostConstruct
+    public void extension(){
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix(System.getenv("TOYBOX_HOME") + File.separator + "ui" + File.separator + "home" + File.separator);
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        templateEngine.addTemplateResolver(resolver);
+    }
+}
---
+++ toybox-server/toybox-ui/src/main/java/com/github/murataykanat/toybox/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.github.murataykanat.toybox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.io.File;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("/resources/", "file:" + System.getenv("TOYBOX_HOME")
+                        + File.separator + "ui" + File.separator
+                        + "home" + File.separator  + "css" + File.separator);
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("/resources/", "file:" + System.getenv("TOYBOX_HOME")
+                        + File.separator + "ui" + File.separator
+                        + "home" + File.separator  + "js" + File.separator);
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/resources/", "file:" + System.getenv("TOYBOX_HOME")
+                        + File.separator + "ui" + File.separator
+                        + "home" + File.separator  + "images" + File.separator);
+    }
+}
