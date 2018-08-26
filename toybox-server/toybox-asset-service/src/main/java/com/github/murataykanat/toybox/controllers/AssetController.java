87005a3522a3c810025b6791f1014d87ba42afcb
Murat Aykanat <aykanatm@gmail.com>
2018-08-26 15:10:15 +0300

    Upload implemented.

--- toybox-server/toybox-asset-service/src/main/java/com/github/murataykanat/toybox/controllers/AssetController.java
+++ toybox-server/toybox-asset-service/src/main/java/com/github/murataykanat/toybox/controllers/AssetController.java
@@ -1,17 +1,39 @@
 package com.github.murataykanat.toybox.controllers;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
+@RefreshScope
 @RestController
 public class AssetController {
-    // The name "upload" must match the "name" attribute of the input in UI
+    private static final Log _logger = LogFactory.getLog(AssetController.class);
+    
+    @Value("${importStagingPath}")
+    private String importStagingPath;
+
+    // The name "upload" must match the "name" attribute of the input in UI (
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public void importAssets(@RequestParam("upload") MultipartFile[] files){
-        for(MultipartFile file: files){
+    public void uploadAssets(@RequestParam("upload") MultipartFile[] files){
+        _logger.debug("uploadAssets() >>");
+        _logger.debug("Import staging path: " + importStagingPath);
+
+        try{
+            for(MultipartFile file: files){
+                file.transferTo(new File(importStagingPath + File.separator + file.getOriginalFilename()));
+            }
             // TODO:
-            // Use config server or environment variable for staging location
-            System.out.println(file.getOriginalFilename());
+            // Start the import job
         }
+        catch (Exception e){
+            String errorMessage = "An error occured while uploading files. " + e.getLocalizedMessage();
+            _logger.error(errorMessage, e);
+        }
+        _logger.debug("<< uploadAssets()");
     }
 }
