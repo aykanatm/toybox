3a34c7659b016d97cfd1a314a5f39744e6425460
Murat Aykanat <aykanatm@gmail.com>
2018-08-26 13:05:13 +0300

    Upload modal window implemented.

---
+++ toybox-server/src/main/config/ui/home/components/import-modal-window/import-modal-window.js
@@ -0,0 +1,84 @@
+const STATUS_INITIAL = 0, STATUS_SAVING = 1, STATUS_SUCCESS = 2, STATUS_FAILED = 3;
+
+module.exports = {
+    data: function() {
+        return  {
+          componentName: 'Import Modal Window',
+          uploadedFiles: [],
+          currentStatus: null,
+          uploadError: null,
+          uploadFieldName: 'upload'
+        }
+    },
+    mounted:function(){
+        this.reset();
+    },
+    computed:{
+        isIntial(){
+            return this.currentStatus === STATUS_INITIAL;
+        },
+        isSaving() {
+            return this.currentStatus === STATUS_SAVING;
+        },
+        isSuccess() {
+            return this.currentStatus === STATUS_SUCCESS;
+        },
+        isFailed() {
+            return this.currentStatus === STATUS_FAILED;
+        }
+    },
+    methods:{
+        reset:function(){
+            this.currentStatus = STATUS_INITIAL;
+            this.uploadedFiles = [];
+            this.uploadError = null;
+        },
+        save(formData){
+            this.currentStatus = STATUS_SAVING;
+            console.log(formData);
+
+            this.upload(formData)
+                .then(x => {
+                    this.uploadedFiles = [].concat(x);
+                    this.currentStatus = STATUS_SUCCESS;
+                })
+                .catch(err => {
+                    this.uploadError = err.response;
+                    this.currentStatus = STATUS_FAILED;
+                });
+        },
+        filesChange(fieldName, fileList){
+            const formData = new FormData();
+
+            if(!fileList.length){
+                return;
+            }
+
+            Array
+                .from(Array(fileList.length).keys())
+                .map(x => {
+                    formData.append(fieldName, fileList[x], fileList[x].name);
+                });
+
+            this.save(formData);
+        },
+        upload(formData){
+            var uploadUrl = 'http://localhost:8101/upload';
+
+            // axios.get(tokenUrl)
+            //     .then((response) => {
+            //         console.log(response);
+            //         console.log('Token: ' + response.data.response);
+            //         var authorization = 'Bearer ' + response.data.response
+            //         console.log(authorization);
+            //         axios.post(uploadUrl, formData, {
+            //             headers:{
+            //                 'Authorization': authorization
+            //             }
+            //         })
+            //     });
+
+            return axios.post(uploadUrl, formData).then(x => x.data);
+        }
+    }
+}
--- toybox-server/src/main/config/ui/home/components/import-modal-window/import-modal-window.vue
+++ toybox-server/src/main/config/ui/home/components/import-modal-window/import-modal-window.vue
@@ -3,22 +3,26 @@
         <i class="close icon"></i>
         <div class="header">Upload Files</div>
         <div class="content toybox-fit-parent toybox-upload-modal-content">
-            <div class="ui middle aligned grid toybox-fit-parent" style="margin-left:0px">
-                <div class="row">
-                    <div class="column">
-                        <div class="ui middle aligned ten column centered grid">
-                            <i class="cloud upload icon toybox-upload-modal-icon"></i>
+            <form class="toybox-fit-parent" enctype="multipart/form-data" novalidate>
+                <div class="ui middle aligned grid toybox-fit-parent" style="margin-left:0px">
+                    <input type="file" multiple style="height:  100%; width: 100%; z-index: 100; opacity:0" v-bind:name="uploadFieldName"
+                    v-bind:disabled="isSaving" v-on:change="filesChange($event.target.name, $event.target.files);">
+                    <div class="row" style="margin-top: -500px;">
+                        <div class="column">
+                            <div class="ui middle aligned ten column centered grid">
+                                <i class="cloud upload icon toybox-upload-modal-icon"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row" style="margin-top: -100px;">
+                        <div class="column">
+                            <div class="ui middle aligned four column centered grid">
+                                <p class="toybox-upload-modal-text">Drag your files here or click to browse</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="column">
-                        <div class="ui middle aligned four column centered grid">
-                            <p class="toybox-upload-modal-text">Drag your files here</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 </template>
