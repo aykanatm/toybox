be7cb0d9c54fd4fb937c0e7b32ef01bbb0132e42
Murat Aykanat <aykanatm@gmail.com>
2018-09-22 15:13:20 +0300

    Replaced environment variable external configuration with the configuration server setting.

--- toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/ThmeleafExtension.java
+++ toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/ThmeleafExtension.java
@@ -1,6 +1,8 @@
 package com.github.murataykanat.toybox.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
@@ -8,15 +10,19 @@ import org.thymeleaf.templateresolver.FileTemplateResolver;
 import javax.annotation.PostConstruct;
 import java.io.File;
 
+@RefreshScope
 @Configuration
 public class ThmeleafExtension {
+    @Value("${toyboxHome}")
+    private String toyboxHome;
+
     @Autowired
     private SpringTemplateEngine templateEngine;
 
     @PostConstruct
     public void extension(){
         FileTemplateResolver resolver = new FileTemplateResolver();
-        resolver.setPrefix(System.getenv("TOYBOX_HOME") + File.separator + "ui" + File.separator + "login" + File.separator);
+        resolver.setPrefix(toyboxHome + File.separator + "ui" + File.separator + "login" + File.separator);
         resolver.setSuffix(".html");
         resolver.setTemplateMode("HTML5");
         resolver.setOrder(templateEngine.getTemplateResolvers().size());
--- toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/WebConfig.java
+++ toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.github.murataykanat.toybox.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,21 +9,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.io.File;
 
+@RefreshScope
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Value("${toyboxHome}")
+    private String toyboxHome;
+
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**")
-                .addResourceLocations("/resources/", "file:" + System.getenv("TOYBOX_HOME")
+                .addResourceLocations("/resources/", "file:" + toyboxHome
                         + File.separator + "ui" + File.separator
                         + "login" + File.separator  + "css" + File.separator);
         registry.addResourceHandler("/js/**")
-                .addResourceLocations("/resources/", "file:" + System.getenv("TOYBOX_HOME")
+                .addResourceLocations("/resources/", "file:" + toyboxHome
                         + File.separator + "ui" + File.separator
                         + "login" + File.separator  + "js" + File.separator);
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("/resources/", "file:" + System.getenv("TOYBOX_HOME")
+                .addResourceLocations("/resources/", "file:" + toyboxHome
                         + File.separator + "ui" + File.separator
                         + "login" + File.separator  + "images" + File.separator);
     }
