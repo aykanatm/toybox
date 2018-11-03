375d9ad14a576e53bc8777e45c816e41121f9711
Murat Aykanat <aykanatm@gmail.com>
2018-11-03 15:19:31 +0300

    Axios and initial CSRF settings added.

--- toybox-server/src/main/config/ui/home/js/files.js
+++ toybox-server/src/main/config/ui/home/js/files.js
@@ -35,6 +35,16 @@ const files = new Vue({
         selectedAssets:[]
     },
     mounted:function(){
+        var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+        var csrfToken = $("meta[name='_csrf']").attr("content");
+        console.log('CSRF header: ' + csrfHeader);
+        console.log('CSRF token: ' + csrfToken);
+        axios.defaults.headers = {
+            // 'X-CSRF-TOKEN': csrfToken,
+            'XSRF-TOKEN': csrfToken
+        }
+        axios.defaults.withCredentials = true;
+
         this.$on('asset-selection-changed', this.onAssetSelectionChanged)
     },
     methods:{
--- toybox-server/src/main/config/ui/home/js/folders.js
+++ toybox-server/src/main/config/ui/home/js/folders.js
@@ -3,6 +3,17 @@ const folders = new Vue({
     data:{
         view: 'folders',
     },
+    mounted:function(){
+        var csrfHeader = $("meta[name='_csrf_header']").attr("content");
+        var csrfToken = $("meta[name='_csrf']").attr("content");
+        console.log('CSRF header: ' + csrfHeader);
+        console.log('CSRF token: ' + csrfToken);
+        axios.defaults.headers = {
+            // 'X-CSRF-TOKEN': csrfToken,
+            'XSRF-TOKEN': csrfToken
+        }
+        axios.defaults.withCredentials = true;
+    },
     components:{
         'navbar' : httpVueLoader('../components/navbar/navbar.vue')
     }
