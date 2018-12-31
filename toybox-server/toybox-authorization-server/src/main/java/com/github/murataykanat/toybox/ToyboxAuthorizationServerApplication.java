bb546b355e59b63921beafeec226df2d7c8e7955
Murat Aykanat <aykanatm@gmail.com>
2018-12-31 15:00:35 +0300

    Eureka configuration applied to all servers and services.

--- toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/ToyboxAuthorizationServerApplication.java
+++ toybox-server/toybox-authorization-server/src/main/java/com/github/murataykanat/toybox/ToyboxAuthorizationServerApplication.java
@@ -2,12 +2,14 @@ package com.github.murataykanat.toybox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableResourceServer
+@EnableEurekaClient
 public class ToyboxAuthorizationServerApplication {
 
 	public static void main(String[] args) {
