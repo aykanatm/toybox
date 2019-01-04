8cb7e2ece7c12a8ce72df3297b709607877c3de8
Murat Aykanat <aykanatm@gmail.com>
2019-01-04 19:33:56 +0300

    Removed unnecessary code and import statements.

--- toybox-server/toybox-job-service/src/main/java/com/github/murataykanat/toybox/models/RetrieveToyboxJobResult.java
+++ toybox-server/toybox-job-service/src/main/java/com/github/murataykanat/toybox/models/RetrieveToyboxJobResult.java
@@ -3,7 +3,6 @@ package com.github.murataykanat.toybox.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
-import java.util.List;
 
 public class RetrieveToyboxJobResult implements Serializable {
     @JsonProperty("toyboxJob")
--- toybox-server/toybox-job-service/src/main/java/com/github/murataykanat/toybox/models/dbo/mappers/ToyboxJobStepRowMapper.java
+++ toybox-server/toybox-job-service/src/main/java/com/github/murataykanat/toybox/models/dbo/mappers/ToyboxJobStepRowMapper.java
@@ -2,16 +2,12 @@ package com.github.murataykanat.toybox.models.dbo.mappers;
 
 import com.github.murataykanat.toybox.models.ToyboxJobStep;
 import com.github.murataykanat.toybox.utilities.DateTimeUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ToyboxJobStepRowMapper implements RowMapper<ToyboxJobStep> {
-    private static final Log _logger = LogFactory.getLog(ToyboxJobStepRowMapper.class);
-
     @Override
     public ToyboxJobStep mapRow(ResultSet resultSet, int i) throws SQLException {
         ToyboxJobStep toyboxJobStep = new ToyboxJobStep();
