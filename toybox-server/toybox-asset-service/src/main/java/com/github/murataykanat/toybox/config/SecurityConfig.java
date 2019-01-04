9d97771e4632e6b05f3bee8d85647334c01c30ec
Murat Aykanat <aykanatm@gmail.com>
2019-01-04 19:23:05 +0300

    Added the missing override annotation. Removed unneeded import statement.

--- toybox-server/toybox-asset-service/src/main/java/com/github/murataykanat/toybox/config/SecurityConfig.java
+++ toybox-server/toybox-asset-service/src/main/java/com/github/murataykanat/toybox/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.github.murataykanat.toybox.config;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,6 +10,7 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableOAuth2Sso
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.antMatcher("/**")
                 .authorizeRequests()
