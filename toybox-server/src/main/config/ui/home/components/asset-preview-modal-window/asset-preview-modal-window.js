b1c28c855a64a57564e33d294e0e1ee59a26339a
Murat Aykanat <aykanatm@gmail.com>
2019-02-03 14:46:16 +0300

    Added the missing this.

--- toybox-server/src/main/config/ui/home/components/asset-preview-modal-window/asset-preview-modal-window.js
+++ toybox-server/src/main/config/ui/home/components/asset-preview-modal-window/asset-preview-modal-window.js
@@ -68,7 +68,7 @@ module.exports = {
     },
     methods:{
         onPreviewImgSrcNotFound:function(){
-            if(!isFirstRendered)
+            if(!this.isFirstRendered)
             {
                 this.hasPreview = false;
             }
