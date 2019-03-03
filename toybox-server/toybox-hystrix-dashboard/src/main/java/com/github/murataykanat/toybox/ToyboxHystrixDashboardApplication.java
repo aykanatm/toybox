84c04d3b6378bcc3b65af2fd251b767412c2e09a
Murat Aykanat <aykanatm@gmail.com>
2019-03-03 13:26:45 +0300

    Fixed the issue where hystrix turbine was not working with the loadbalancer application.

--- toybox-server/toybox-hystrix-dashboard/src/main/java/com/github/murataykanat/toybox/ToyboxHystrixDashboardApplication.java
+++ toybox-server/toybox-hystrix-dashboard/src/main/java/com/github/murataykanat/toybox/ToyboxHystrixDashboardApplication.java
@@ -3,7 +3,9 @@ package com.github.murataykanat.toybox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableTurbine
 @SpringBootApplication
 @EnableHystrixDashboard
 public class ToyboxHystrixDashboardApplication {
