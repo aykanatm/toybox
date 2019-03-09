6e038f95a9568fe47c754bbc206c03ccf73ff8f3
Murat Aykanat <aykanatm@gmail.com>
2019-03-09 17:39:48 +0300

    Added more exception handling for the load balancer.

--- toybox-server/toybox-rendition-loadbalancer/src/main/java/com/github/murataykanat/toybox/controllers/RenditionLoadbalancerController.java
+++ toybox-server/toybox-rendition-loadbalancer/src/main/java/com/github/murataykanat/toybox/controllers/RenditionLoadbalancerController.java
@@ -1,6 +1,7 @@
 package com.github.murataykanat.toybox.controllers;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,50 +39,138 @@ public class RenditionLoadbalancerController {
     public ResponseEntity<Resource> getLoadBalancedUserAvatar(HttpSession session, @PathVariable String username){
         _logger.debug("getLoadBalancedUserAvatar() >>");
 
-        _logger.debug("Session ID: " + session.getId());
-        CsrfToken token = (CsrfToken) session.getAttribute("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN");
-        _logger.debug("CSRF Token: " + token.getToken());
+        if(StringUtils.isNotBlank(username)){
+            if(session != null){
+                _logger.debug("Session ID: " + session.getId());
+                CsrfToken token = (CsrfToken) session.getAttribute("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN");
+                if(token != null){
+                    _logger.debug("CSRF Token: " + token.getToken());
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Cookie", "SESSION=" + session.getId() + "; XSRF-TOKEN=" + token.getToken());
-        headers.set("X-XSRF-TOKEN", token.getToken());
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.set("Cookie", "SESSION=" + session.getId() + "; XSRF-TOKEN=" + token.getToken());
+                    headers.set("X-XSRF-TOKEN", token.getToken());
 
-        _logger.debug("<< getLoadBalancedUserAvatar()");
-        return restTemplate.exchange("http://toybox-rendition-service/renditions/users/" + username, HttpMethod.GET, new HttpEntity<>(headers), Resource.class);
+                    _logger.debug("<< getLoadBalancedUserAvatar()");
+                    return restTemplate.exchange("http://toybox-rendition-service/renditions/users/" + username, HttpMethod.GET, new HttpEntity<>(headers), Resource.class);
+                }
+                else{
+                    String errorMessage = "CSRF token is null!";
+                    _logger.error(errorMessage);
+
+                    _logger.debug("<< getLoadBalancedUserAvatar()");
+                    return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+                }
+            }
+            else{
+                String errorMessage = "Session is null!";
+                _logger.error(errorMessage);
+
+                _logger.debug("<< getLoadBalancedUserAvatar()");
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        }
+        else{
+            String errorMessage = "Username is blank!";
+            _logger.error(errorMessage);
+
+            _logger.debug("<< getLoadBalancedUserAvatar()");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public ResponseEntity<Resource> userRenditionErrorFallback(HttpSession session, @PathVariable String username){
+        _logger.debug("userRenditionErrorFallback() >>");
+
+        if(StringUtils.isNotBlank(username)){
+            _logger.error("Username is blank!");
+
+            _logger.debug("<< userRenditionErrorFallback()");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        else{
+            _logger.error("Unable to retrieve rendition for username '" + username + "'. Please check if the any of the rendition services are running.");
+
+            _logger.debug("<< userRenditionErrorFallback()");
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @HystrixCommand(fallbackMethod = "assetRenditionErrorFallback")
     @RequestMapping(value = "/renditions/assets/{assetId}/{renditionType}", method = RequestMethod.GET, produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<Resource> getLoadBalancedRendition(HttpSession session, @PathVariable String assetId, @PathVariable String renditionType){
         _logger.debug("getLoadBalancedRendition() >>");
+        if(StringUtils.isNotBlank(assetId)){
+            if(StringUtils.isNotBlank(renditionType)){
+                if(session != null){
+                    _logger.debug("Session ID: " + session.getId());
+                    CsrfToken token = (CsrfToken) session.getAttribute("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN");
+                    if(token != null){
+                        _logger.debug("CSRF Token: " + token.getToken());
 
-        _logger.debug("Session ID: " + session.getId());
-        CsrfToken token = (CsrfToken) session.getAttribute("org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository.CSRF_TOKEN");
-        _logger.debug("CSRF Token: " + token.getToken());
+                        HttpHeaders headers = new HttpHeaders();
+                        headers.set("Cookie", "SESSION=" + session.getId() + "; XSRF-TOKEN=" + token.getToken());
+                        headers.set("X-XSRF-TOKEN", token.getToken());
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Cookie", "SESSION=" + session.getId() + "; XSRF-TOKEN=" + token.getToken());
-        headers.set("X-XSRF-TOKEN", token.getToken());
+                        _logger.debug("<< getLoadBalancedRendition()");
+                        return restTemplate.exchange("http://toybox-rendition-service/renditions/assets/" + assetId + "/" + renditionType, HttpMethod.GET, new HttpEntity<>(headers), Resource.class);
+                    }
+                    else{
+                        String errorMessage = "CSRF token is null!";
+                        _logger.error(errorMessage);
 
-        _logger.debug("<< getLoadBalancedRendition()");
-        return restTemplate.exchange("http://toybox-rendition-service/renditions/assets/" + assetId + "/" + renditionType, HttpMethod.GET, new HttpEntity<>(headers), Resource.class);
-    }
+                        _logger.debug("<< getLoadBalancedRendition()");
+                        return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+                    }
+                }
+                else{
+                    String errorMessage = "Session is null!";
+                    _logger.error(errorMessage);
 
-    public ResponseEntity<Resource> userRenditionErrorFallback(HttpSession session, @PathVariable String username){
-        _logger.debug("userRenditionErrorFallback() >>");
+                    _logger.debug("<< getLoadBalancedRendition()");
+                    return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+                }
+            }
+            else{
+                String errorMessage = "Rendition type is blank!";
+                _logger.error(errorMessage);
 
-        _logger.error("Unable to retrieve rendition for username '" + username + "'. Please check if the any of the rendition services are running.");
+                _logger.debug("<< getLoadBalancedRendition()");
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        else{
+            String errorMessage = "Asset ID is blank!";
+            _logger.error(errorMessage);
 
-        _logger.debug("<< userRenditionErrorFallback()");
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            _logger.debug("<< getLoadBalancedRendition()");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     public ResponseEntity<Resource> assetRenditionErrorFallback(HttpSession session, @PathVariable String assetId, @PathVariable String renditionType){
         _logger.debug("assetRenditionErrorFallback() >>");
 
-        _logger.error("Unable to retrieve " +  renditionType + " rendition for asset with ID '" + assetId + "'. Please check if the any of the rendition services are running.");
+        if(StringUtils.isNotBlank(assetId)){
+            if(StringUtils.isNotBlank(renditionType)){
+                _logger.error("Unable to retrieve " +  renditionType + " rendition for asset with ID '" + assetId + "'. Please check if the any of the rendition services are running.");
 
-        _logger.debug("<< assetRenditionErrorFallback()");
-        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                _logger.debug("<< assetRenditionErrorFallback()");
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            else{
+                String errorMessage = "Rendition type is blank!";
+                _logger.error(errorMessage);
+
+                _logger.debug("<< assetRenditionErrorFallback()");
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
+        else{
+            String errorMessage = "Asset ID is blank!";
+            _logger.error(errorMessage);
+
+            _logger.debug("<< assetRenditionErrorFallback()");
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
