5d3ea7a2ccb976197177b1a62ed1cd02b1afc9a6
Murat Aykanat <aykanatm@gmail.com>
2019-04-30 17:52:36 +0300

    To support all browsers, download function is now handled by the FileSaver library.

--- toybox-server/src/main/config/ui/home/components/mixins/asset-actions/asset-actions-mixin.js
+++ toybox-server/src/main/config/ui/home/components/mixins/asset-actions/asset-actions-mixin.js
@@ -115,33 +115,16 @@ var assetActionsMixin = {
                             .then(response =>{
                                 console.log(response);
                                 var filename;
-                                var mimeType;
 
                                 if(assets.selectedAssets.length == 1){
                                     filename = assets.selectedAssets[0].name;
-                                    mimeType = assets.selectedAssets[0].type;
                                 }
                                 else{
                                     filename = 'Download.zip';
-                                    mimeType = 'application/zip';
                                 }
 
-                                if (typeof window.chrome !== 'undefined') {
-                                    // Chrome version
-                                    var link = document.createElement('a');
-                                    link.href = window.URL.createObjectURL(response.data);
-                                    link.download = filename;
-                                    document.body.appendChild(link)
-                                    link.click();
-                                } else if (typeof window.navigator.msSaveBlob !== 'undefined') {
-                                    // IE version
-                                    var blob = new Blob([response.data], { type: mimeType });
-                                    window.navigator.msSaveBlob(blob, filename);
-                                } else {
-                                    // Firefox version
-                                    var file = new File([response.data], filename, { type: 'application/force-download' });
-                                    window.open(URL.createObjectURL(file));
-                                }
+                                var blob = new Blob([response.data], {type:'application/octet-stream'});
+                                saveAs(blob , filename);
                             })
                             .catch(error => {
                                 var errorMessage;
