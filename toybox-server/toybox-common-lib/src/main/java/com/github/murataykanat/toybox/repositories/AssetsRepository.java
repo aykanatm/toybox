5593a10368719a3e8552780215400794c3b48048
Murat Aykanat <aykanatm@gmail.com>
2019-05-25 15:14:28 +0300

    Changed the query to exclude the deleted assets.

--- toybox-server/toybox-common-lib/src/main/java/com/github/murataykanat/toybox/repositories/AssetsRepository.java
+++ toybox-server/toybox-common-lib/src/main/java/com/github/murataykanat/toybox/repositories/AssetsRepository.java
@@ -20,7 +20,7 @@ public interface AssetsRepository extends JpaRepository<Asset, String> {
     @Query(value = "SELECT asset_id, asset_extension, asset_import_date, asset_imported_by_username, asset_name, asset_path, asset_preview_path, asset_thumbnail_path, asset_type, deleted, checksum, is_latest_version, original_asset_id, version, file_size FROM assets WHERE asset_name=?1 AND asset_imported_by_username=?2", nativeQuery = true)
     List<Asset> getDuplicateAssetsByAssetNameAndUsername(String assetName, String username);
 
-    @Query(value = "SELECT asset_id, asset_extension, asset_import_date, asset_imported_by_username, asset_name, asset_path, asset_preview_path, asset_thumbnail_path, asset_type, deleted, checksum, is_latest_version, original_asset_id, version, file_size FROM assets WHERE original_asset_id=?1", nativeQuery = true)
+    @Query(value = "SELECT asset_id, asset_extension, asset_import_date, asset_imported_by_username, asset_name, asset_path, asset_preview_path, asset_thumbnail_path, asset_type, deleted, checksum, is_latest_version, original_asset_id, version, file_size FROM assets WHERE original_asset_id=?1 AND deleted='N'", nativeQuery = true)
     List<Asset> getAssetsByOriginalAssetId(String originalAssetId);
 
     @Modifying
