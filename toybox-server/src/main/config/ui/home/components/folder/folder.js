7b2d89043fc29c72efc1b7ef3b11ae0fe7c38cc9
Murat Aykanat <aykanatm@gmail.com>
2019-08-14 17:32:21 +0300

    TOYB-11 - Fixed a bug where single folder download via context menu was not working.

--- toybox-server/src/main/config/ui/home/components/folder/folder.js
+++ toybox-server/src/main/config/ui/home/components/folder/folder.js
@@ -70,6 +70,10 @@ module.exports = {
             var folder = {
                 id: this.id,
                 name: this.name,
+                type: this.type,
+                originalAssetId: this.originalAssetId,
+                '@class': 'com.github.murataykanat.toybox.dbo.Container',
+                parentContainerId: this.parentContainerId
             }
             var selectedFolders = [folder];
             this.downloadItems(selectedFolders);
