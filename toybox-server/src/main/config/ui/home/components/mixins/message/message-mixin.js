649c2fafcf3c0a4c6006b4e65189b8fc3010d307
Murat Aykanat <aykanatm@gmail.com>
2019-10-13 13:24:38 +0300

    TOYB-51 - Converted messages into toast widget

--- toybox-server/src/main/config/ui/home/components/mixins/message/message-mixin.js
+++ toybox-server/src/main/config/ui/home/components/mixins/message/message-mixin.js
@@ -1,50 +1,15 @@
 var messageMixin = {
-    data:function(){
-        return{
-            messages: [],
-        }
-    },
     methods:{
         displayMessage:function(type, messageString){
-            var isError = false;
-            var isOk = false;
-            var isWarning = false;
-            var isInfo = false;
-
-            if(type === 'Information'){
-                isInfo = true;
-            }
-            else if(type === 'Error'){
-                isError = true;
-            }
-            else if(type === 'Warning'){
-                isWarning = true;
-            }
-            else if(type === 'Success'){
-                isOk = true;
-            }
-            else{
-                isInfo = true;
-            }
-
-            var message = {id:this.messages.length, isError:isError, isOk:isOk, isWarning:isWarning, isInfo:isInfo, header:type, message:messageString}
-            this.messages.push(message);
-            // Initialize events on the message
-            setTimeout(() => {
-                $('.message .close')
-                    .on('click', function() {
-                        $(this)
-                        .closest('.message')
-                        .remove();
-                    });
-
-                var messageSelector = '#' + message.id + '.message';
-                $(messageSelector)
-                    .delay(2000)
-                    .queue(function(){
-                        $(this).remove().dequeue();
-                    });
-            }, 100);
+            var typeClass = type === 'Information' ? 'info' : type.toLowerCase()
+            $('body')
+                .toast({
+                    title: type,
+                    class: typeClass,
+                    message: messageString,
+                    position: 'top center',
+                    displayTime: 5000
+                });
         },
     }
 }
